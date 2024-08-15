7bb832e4bd06a2753aee85284ecca38df5e3a4c9
cross <kalleouchirra@gmail.com>
2024-08-15 14:17:52 -0300

    Add files via upload

---
+++ trabalho js/index.js
@@ -0,0 +1,77 @@
+//DIA 1 com javascript nivel 2
+
+
+/*let aluno = {nome: "kalleo", idade: "15", curso: "3000 talentos"}
+let alunos = [aluno]
+
+console.log(alunos);*/
+
+/*let soma = (5 + 7)
+
+console.log(soma);*/
+
+/*let teste = (5 > 6)
+
+console.log(teste)*/
+
+/*function soma(num) {
+    return num >= 10 && num <=20;
+}
+
+
+
+
+
+
+DIA 2 com javascript nivel 2
+
+
+console.log(soma(15))
+console.log(soma(9))*/
+
+/*let idade = 18;
+
+if (idade >= 21) {
+    console.log("Maior de idade");
+} else if (idade >= 5) {
+    console.log("idosa");
+}*/
+
+
+
+/*let diaSemana = 3;
+
+switch(diaSemana) {
+    case 1:
+        console.log("A");
+        break;
+    case 2:
+        console.log("B");
+        break;
+    case 3:
+        console.log("C");
+        break;
+    case 4:
+        console.log("D")
+        break;
+    case 5:
+        console.log("E")
+    default:
+        console.log("Dia inválido");
+}*/
+
+
+
+/*for (let i = 0; i < 9; i++) {
+    console.log(i);
+}*/
+
+
+const multiplica = function(a, b) {
+    return a * b;
+};
+
+const divide = (a, b) => a / b;
+
+console.log(multiplica(5, 3));  // 15
+console.log(divide(15, 3));  // 5
